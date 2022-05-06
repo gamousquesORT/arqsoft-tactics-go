815a4a867bca9ff511c3870f44579ad413d2d6ae
gamousquesORT <mousques@ort.edu.uy>
2022-05-06 18:50:34 -0300

    ejecutando ok, necesita refactoreo para que esté mas clean

--- performance/concurrencia/pipes-filters/website-pipeline.go
+++ performance/concurrencia/pipes-filters/website-pipeline.go
@@ -1,21 +1,10 @@
 /*
-	este ejemplo es el mismo que sync_v1 pero utilizando goroutines como funciones anónimas.
-		ej.  	go func (u string, workerId int)  {
+	este ejemplo utiliza canales para sincronizar las gorutinas y muestra el uso
+	del patrón pipeline y de sincronización Fan-out (varios gorutinas leyendo de un mismo canal), 
+	fan-in (un funcion lee de varias fuentes y las multiplexa en un canal)
 
-	Go cuenta con dos grupos de mecanismos de concurrencia.
-	el paquete syn que permite forkear goroutines y hacer join de su ejecución
-	sync también cuenta con semáforos (mutex y variantes) para proteger variables o "memoria" que
-	comparten las goroutines. En este caso las gorrutinas no comparten información.
 
-	Por ejemplo si quisiéramos utilizar una slice o map para guardar los resutados que se
-	despliegan cada llamada a CheckOneWebsite(...) esa estructura deberia estar protegida
-	por un sync.Mutex
-
-	Este ejemplo solamente utiliza el mecanismo de fork y join que se conoce como WaitGroup
-	el WaitGroup permite
-	  - llevar el contardor de goroutinas a sincronizar Add(n)
-	  - decrementar el contador cuando una goroutina termina Done()
-	  - esperar que todas las goroutinas en ejecución terminen Wait()
+		https://go.dev/blog/pipelines
 
 	puede ejecutarlo con
 		go run . para ejecutar el main o
@@ -35,43 +24,87 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 )
 
 // esta función es la que llama a las goroutinas y las sincroniza
 // utilizando sync.WaitGroups
-func CheckWebsite(in <-chan string) <-chan string {
+func checkWebsite(done <- chan struct {}, in <-chan string) <-chan string {
 
 	out := make(chan string)
-
-
 	go func() {
-
+		defer close(out)
 		for url := range in {
-
-			// el que sigue es el mismo código que en el ejemplo secuencial
-			response, err := http.Head(url)
-			if err != nil {
-				out <- fmt.Sprintf("El url %s no existe \n", url)
-			} else {
-				// ok Head sin error, si no es ok retorna url:false si es ok url:true
-				if response.StatusCode != http.StatusOK {
-					out <- fmt.Sprintf("El url %s NO responde OK \n", url)
-				} else {
-					out <- fmt.Sprintf("El url %s responde OK \n", url)
-				}
+			select {
+			case out <- callHead(url):
+			case <- done:
+				return	
 			}
 		}
-		close(out)
 	}()
-
-
-		return out
+	
+	return out
 
 }
 
-func FeedWebsites() <-chan string {
-	out := make(chan string)
+func callHead(url string) string {
+	var ret string
 
+	// el que sigue es el mismo código que en el ejemplo secuencial
+	response, err := http.Head(url)
+	if err != nil {
+		 ret = fmt.Sprintf("El url %s no existe \n", url)
+	} else {
+		// ok Head sin error, si no es ok retorna url:false si es ok url:true
+		if response.StatusCode != http.StatusOK {
+			ret = fmt.Sprintf("El url %s NO responde OK \n", url)
+		} else {
+			ret = fmt.Sprintf("El url %s responde OK \n", url)
+		}
+	}
+	return ret
+}
+
+func merge(done <-chan struct{}, cs... <-chan string) <-chan string {
+    var wg sync.WaitGroup
+    out := make(chan string)
+
+	// esta gorutina saca de los canales de entrada y pasa lo recibido a un canal único (out)
+	// notar que en 2do argumento de merge es un colección de canales
+	// se usa el waitgroup para saber cuantos canales están aportando al merge
+    output := func(c <-chan string) {
+		defer wg.Done()
+        for n := range c {
+            select {
+            case out <- n:
+            case <-done:
+                return
+            }
+        }
+    }
+
+	wg.Add(len(cs))
+
+	// se ejecuta la gorutina anterior pasando cada canal
+    for _, c := range cs {
+        go output(c)
+    }
+
+	// se ejecuta una gorutina que cierra el canal último cundo 
+	// las anteriores teriminaron (los sabe por el <- done del select y el wait)
+    go func() {
+        wg.Wait()
+        close(out)
+    }()
+
+    return out
+
+}
+
+
+func feedWebsites(done <- chan struct{}) <-chan string {
+	out := make(chan string)
+	
 	var websites = []string{
 		"http://ort.edu.uy",
 		"http://google.com",
@@ -82,21 +115,38 @@ func FeedWebsites() <-chan string {
 		"http://ingsoft.gaston.com",
 	}
 	go func ()  {
+		defer close(out)
 		for _, ws := range websites {
-			out <- ws
+			select {
+			case out <- ws:
+			case <-done:
+				return
+			}
 		}
-		close(out)
-		
 	}()
-
+	
 	return out
 }
 
 func WebsiteStatusChecker()	{
 
-	for n:= range CheckWebsite(FeedWebsites()) {
-		fmt.Printf("%s", n)
+	//se crea un canal para comunicar que las gorutinas fueron terminando su trabajo
+	done := make(chan struct{})
+	defer close(done)
+
+
+	in := feedWebsites(done)
+
+	firstFeeder := checkWebsite(done, in)
+	secondFeeder := checkWebsite(done, in)
+
+	// función que mergea lo que pasa en los dos canales de entrada en uno de salida 
+	out := merge(done, firstFeeder, secondFeeder)
+
+	for n := range out {
+		fmt.Printf(n)
 	}
+	
 }
 
 
